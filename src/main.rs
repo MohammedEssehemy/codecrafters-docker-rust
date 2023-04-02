59d8f04a26acb7fd011e044639c5c7ea301682fc
Mohammed Essehemy <mohammedessehemy@gmail.com>
2023-04-02 12:04:20 +0400

    feat: pipe stdout & stderr

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use anyhow::{Context, Result};
-use std::process::{self, Command};
-use std::str;
+use std::process::Command;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -10,23 +9,15 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    let output = Command::new(command)
+    Command::new(command)
         .args(command_args)
-        .output()
+        .spawn()
         .with_context(|| {
             format!(
                 "Tried to run '{}' with arguments {:?}",
                 command, command_args
             )
         })?;
-
-    let std_out = str::from_utf8(&output.stdout)?;
-    if output.status.success() {
-        println!("{}", std_out);
-    } else {
-        eprintln!("{std_out}");
-        process::exit(1);
-    }
 
     Ok(())
 }

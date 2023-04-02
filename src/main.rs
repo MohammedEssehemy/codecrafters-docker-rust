0339dd346e8931b7b80442750d62a06869710d83
Mohammed Essehemy <mohammedessehemy@gmail.com>
2023-04-02 11:50:06 +0400

    feat: run program based on input

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 use anyhow::{Context, Result};
+use std::process::{self, Command};
+use std::str;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage!
-    // let args: Vec<_> = std::env::args().collect();
-    // let command = &args[3];
-    // let command_args = &args[4..];
-    // let output = std::process::Command::new(command)
-    //     .args(command_args)
-    //     .output()
-    //     .with_context(|| {
-    //         format!(
-    //             "Tried to run '{}' with arguments {:?}",
-    //             command, command_args
-    //         )
-    //     })?;
-    //
-    // if output.status.success() {
-    //     let std_out = std::str::from_utf8(&output.stdout)?;
-    //     println!("{}", std_out);
-    // } else {
-    //     std::process::exit(1);
-    // }
+    let args: Vec<_> = std::env::args().collect();
+    let command = &args[3];
+    let command_args = &args[4..];
+    let output = Command::new(command)
+        .args(command_args)
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
+
+    let std_out = str::from_utf8(&output.stdout)?;
+    if output.status.success() {
+        println!("{}", std_out);
+    } else {
+        eprintln!("{std_out}");
+        process::exit(1);
+    }
 
     Ok(())
 }

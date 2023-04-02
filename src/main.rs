8748a09205a2da53e15d1e3e448f63e525d6a6af
Mohammed Essehemy <mohammedessehemy@gmail.com>
2023-04-02 12:31:16 +0400

    feat: handle exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use std::process::Command;
+use std::process::{self, Command};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -9,7 +9,7 @@ fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
-    Command::new(command)
+    let mut child = Command::new(command)
         .args(command_args)
         .spawn()
         .with_context(|| {
@@ -18,6 +18,6 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-
-    Ok(())
+    let exist_status = child.wait()?;
+    process::exit(exist_status.code().unwrap_or(1));
 }
